387295bac2f62d124f1ead2cfeec11610612e433
zidantech <zidantech4@gmail.com>
2022-09-08 15:06:09 -0700

    c is fun

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Prints a text
+ *
+ * Return: value 0
+ * */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n")
+		return(0);
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the size of many var types
+ *
+ * Return: value 0
+ * */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+
+	printf("Size of a char:%d byte(s)\n", sizeof(a));
+	printf("Size of a int:%d byte(s)\n", sizeof(b));
+	printf("Size of a long int:%d byte(s)\n", sizeof(c));
+	printf("Size of a long long int:%d byte(s)\n", sizeof(d));
+	printf("Size of a float:%d byte(s)\n", sizeof(e));
+	return (0);
+}
