79eeee26b36ecb34fb2a663223d367512cee357e
zidantech <zidantech4@gmail.com>
2022-09-10 01:07:07 +0100

    task done

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - Entry point 
+ * main - Entry point
  * Description: prints numbers
- * Return - Always 0
+ * Return: Always 0
  */
 int main(void)
 {
