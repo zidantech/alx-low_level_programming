2175655b550f7a7379bdffd40c3ffec6be04b816
zidantech <zidantech4@gmail.com>
2022-09-10 01:35:47 +0100

    doing advance

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,9 +8,9 @@ int main(void)
 {
 	int c, i, k;
 
-	for (c = '0'; c <= '9'; i++)
+	for (c = '0'; c <= '9'; c++)
 	{
-		for (i = 0; c <= 9; c++)
+		for (i = 0; c <= 9; i++)
 		{
 			for (k = '0'; k <= '9'; k++)
 			{
